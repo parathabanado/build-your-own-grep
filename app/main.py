cc9b6d23e2fa0eeae8bf28d6ab8774e9e3700163
parathabanado <parathacodes@gmail.com>
2024-08-11 17:22:51 +0530

    Stage 2 Digit Check for Grep

--- app/main.py
+++ app/main.py
@@ -4,9 +4,14 @@ import sys
 # import lark - available if you need it!
 
 
+
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
+    elif len(pattern) == 2:
+        if pattern=="\d":
+            return len(input_line.translate(str.maketrans('','','0123456789'))) != len(input_line)
+
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
@@ -14,7 +19,8 @@ def match_pattern(input_line, pattern):
 def main():
     pattern = sys.argv[2]
     input_line = sys.stdin.read()
-
+    print(pattern)
+    print(input_line)
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
